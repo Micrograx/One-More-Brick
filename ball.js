bdbe54fca584e11a601a2b4a858b07c3f6155b66
Valentino <valentinocerutti@gmail.com>
2017-12-27 12:15:33 -0300

    Full collision and bounce code

--- ball.js
+++ ball.js
@@ -1,53 +1,87 @@
 function ball(x,y,d) {
 
-this.pos = createVector(x,y);
-this.velocity = createVector(0,0);
-this.acceleration = createVector(0,0);
-this.maxspeed = 10;
+	this.pos = createVector(x,y);
+	this.velocity = createVector(0,0);
+	this.acceleration = createVector(0,0);
+	this.maxspeed = (d / 2) - 2;
 
-this.size = d;
+	this.size = d;
 
-this.update = function(){
+	this.update = function(){
 
-	this.velocity.add(this.acceleration);
-  this.velocity.limit(this.maxspeed);
-  this.pos.add(this.velocity);
-  this.acceleration.mult(0);
+		this.velocity.add(this.acceleration);
+	  this.velocity.limit(this.maxspeed);
+	  this.pos.add(this.velocity);
+	  this.acceleration.mult(0);
 
-	// Bounce when touch the edge of the canvas
-	if (this.pos.x < 0) {
-		this.pos.x = 0;
-		this.velocity.x = -this.velocity.x;
+		// Bounce when touch the edge of the canvas
+		if (this.pos.x < 0) {
+			this.pos.x = 0;
+			this.velocity.x = -this.velocity.x;
+		}
+	 	if (this.pos.y < 0) {
+	 		this.pos.y = 0;
+	 		this.velocity.y = -this.velocity.y;
+	 	}
+	 	if (this.pos.x > width - this.size) {
+	 		this.pos.x = width - this.size;
+	 		this.velocity.x = -this.velocity.x;
+	 	}
+	 	if (this.pos.y > height - this.size) {
+	 		this.pos.y = height - this.size;
+	 		this.velocity.mult(0);
+	 	}
 	}
- 	if (this.pos.y < 0) {
- 		this.pos.y = 0;
- 		this.velocity.y = -this.velocity.y;
- 	}
- 	if (this.pos.x > width - this.size) {
- 		this.pos.x = width - this.size;
- 		this.velocity.x = -this.velocity.x;
- 	}
- 	if (this.pos.y > height - this.size) {
- 		this.pos.y = height - this.size;
- 		this.velocity.mult(0);
- 	}
-}
 
-this.check = function(){
-	if (this.pos.y >= height - this.size ){
-		if (xPos == null){
-			xPos = this.pos.x
-		} else {
-			this.pos.x = xPos
+	this.check = function(){
+		if (this.pos.y >= height - this.size ){
+			if (xPos == null){
+				xPos = this.pos.x
+			} else {
+				this.pos.x = xPos
+			}
 		}
 	}
-}
 
-this.draw = function(){
-  ellipse(this.pos.x,this.pos.y,this.size);
-}
+	this.collision = function(){
 
-this.applyForce = function(x,y){
-  this.acceleration = createVector(x,y);
-}
+		this.i = floor(this.pos.y / blockSize)
+		this.j = floor(this.pos.x / blockSize)
+		this.collisioning = []
+
+		changeX = false
+		changeY = false
+
+		for (let i = 0; i < bloques.length; i++){
+				hit = collideRectCircle(bloques[i].pos.x,bloques[i].pos.y,bloques[i].size,bloques[i].size,this.pos.x,this.pos.y,this.size)
+				if (hit) {
+					this.collisioning.push(bloques[i])
+					// console.log("bloque: " + bloques[i].j + " " + bloques[i].i)
+					// console.log("ball: " + this.j + " " + this.i)
+				}
+		}
+
+
+		for (let i = 0; i < this.collisioning.length; i++){
+			if (((this.i - 1 ) == this.collisioning[i].i) || ((this.i + 1) == this.collisioning[i].i)){
+				changeY = true
+				this.collisioning[i].hited()
+			} else if ((((this.j + 1) == this.collisioning[i].j) || ((this.j - 1) == this.collisioning[i].j)) && (this.i == this.collisioning[i].i)) {
+				changeX = true
+				this.collisioning[i].hited()
+			}
+		}
+
+		if (changeX) this.velocity.x = -this.velocity.x
+		if (changeY) this.velocity.y = -this.velocity.y
+
+	}
+
+	this.draw = function(){
+  	ellipse(this.pos.x,this.pos.y,this.size);
+	}
+
+	this.applyForce = function(x,y){
+  	this.acceleration = createVector(x,y);
+	}
 }
--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ let pointC = 0;
 let forceA = 0;
 let forceB = 0;
 
-let state = "start";
+let state = "waiting";
 
 let xPos = null
 
@@ -31,7 +31,10 @@ function setup(){
   cnv = createCanvas(420,580);
   cnv.mousePressed(createPointA);
   cnv.mouseReleased(createPointB);
-  rectMode(CENTER)
+
+
+  frameRate(60)
+  //rectMode(CENTER)
 
   blockSize = (window.width / columnas) - 1;
 
@@ -39,7 +42,7 @@ function setup(){
     balls.push(new ball(window.width / 2, 540, ballSize));
   }
 
-  createBlocks(1)
+  createBlocks(0.5)
 
 }
 
@@ -47,10 +50,20 @@ function draw(){
   background(150);
   for (let i = 0; i < cantBalls; i++){
     balls[i].update();
+    balls[i].collision()
     balls[i].draw();
+
   }
 
   for (let i = 0; i < bloques.length; i++){
+    if (bloques[i].health <= 0) {
+      bloques.splice(i,1);
+      break
+    }
+    if (bloques[i].i == 8){
+      state = "GAME OVER"
+      console.log(state)
+    }
     bloques[i].draw();
   }
 
@@ -72,19 +85,30 @@ function draw(){
     }
   }
 
+  //Collision and bounce code
+
+
+  let moving = 0
   //Determina el estado del juego
-  if (!balls[0].velocity.equals(createVector(0,0)) && balls[cantBalls - 1].velocity.equals(createVector(0,0))){
+  for (let i = 0; i < balls.length; i++){
+    if (!balls[i].velocity.equals(createVector(0,0))){
+      moving += 1
+    }
+  }
+
+
+  if (moving < balls.length && state == "waiting" && moving != 0){
     state = "shooting"
-  } else if (!balls[cantBalls - 1].velocity.equals(createVector(0,0))) {
+  } else if (moving == balls.length) {
     state = "moving"
-  } else if ((balls[cantBalls - 1].velocity.equals(createVector(0,0))) && state == "moving") {
+  } else if (moving == 0 && state != "waiting" && state != "GAME OVER") {
     state = "finished"
     nuevoNivel()
   }
 
 
   //Dibuja la linea de disparo
-  if (mouseIsPressed && state != "shooting"){
+  if (mouseIsPressed && state == "waiting"){
     pointB = createVector(mouseX,mouseY);
     pointC = p5.Vector.sub(pointA,pointB)
     ballPosA = balls[0].pos
@@ -104,14 +128,14 @@ function shoot(dir){
 
 //Crea el primer punto para disparar las bolas
 function createPointA(){
-  if (state != "shooting"){
+  if (state == "waiting"){
     pointA = createVector(mouseX,mouseY);
     pointB = 0;
   }
 }
 //Determina el segundo punto para disparar y llama a shoot()
 function createPointB(){
-  if (state != "shooting"){
+  if (state == "waiting"){
     shoot(pointC);
   }
 }
@@ -120,21 +144,24 @@ function createBlocks(prob){
   for (let j = 0; j < columnas; j++){ //crear fila
     let r = Math.random(0,1)
     if (r < prob){
-        bloques.push(new bloque(1,j,blockSize))
+        bloques.push(new bloque(1,j,blockSize,level))
     }
   }
 }
 
 function nuevoNivel(){
   state = "waiting"
+  level += 1
+
   for (let h = 0; h < bloques.length; h++){
     bloques[h].i += 1
   }
-  createBlocks(0.8)
-  level += 1
+  createBlocks(0.6)
   balls.push(new ball(xPos,(height - ballSize), ballSize));
   cantBalls += 1
   xPos = null
+
+  console.log("NEW LEVEL")
 
 
 }
